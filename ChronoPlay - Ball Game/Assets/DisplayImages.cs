11046fc0c02a3db5259e6ead3bd3f7f50ccaca57
hd910 <haydendo910@hotmail.com>
2015-11-25 16:22:00 +1300

    Added class to get images from url + created placeholder picture for each hole

---
+++ ChronoPlay - Ball Game/Assets/DisplayImages.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Get the latest webcam shot from outside "Friday's" in Times Square
+public class DisplayImages : MonoBehaviour
+{
+    public string url = "http://images.earthcam.com/ec_metros/ourcams/fridays.jpg";
+
+    IEnumerator Start()
+    {
+        // Start a download of the given URL
+        WWW www = new WWW(url);
+
+        // Wait for download to complete
+        yield return www;
+
+        // assign texture
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = www.texture;
+    }
+}
