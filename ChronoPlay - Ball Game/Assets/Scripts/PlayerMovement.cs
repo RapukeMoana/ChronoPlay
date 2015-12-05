0fcd87d5e8d028cf9ec2d702b06e34b41539b2b3
hd910 <hd910@outlook.com>
2015-12-05 18:08:23 +1300

    Added result

--- ChronoPlay - Ball Game/Assets/Scripts/PlayerMovement.cs
+++ ChronoPlay - Ball Game/Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour {
     public float speed;
     public float plateDistance;
-    private Rigidbody rb;
-    private int level;
-    
 
-	// Use this for initialization
-	void Start () {
+    private Rigidbody rb;
+    private float speedsmooth = 0.8f;
+    private float myAlpha = 1.0f;
+    private bool resultFade = true;
+
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody>();
         plateDistance = 20f;
-        level = 0;
-	}
+
+        myAlpha = 1.0f; // maybe you need other value
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
@@ -26,19 +30,49 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     void OnCollisionEnter(Collision other) {
+        Text scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        GameObject result;
         //Checks to see which wormhole ball enters 
         switch (other.transform.tag)
         {
+            //If correct hole, update score and show result
             case "Correct":
-                print("Correct");
                 Destroy(other.gameObject);
+                scoreText.text = "";
+                result = (GameObject)Instantiate(Resources.Load("Result"));
+                GameObject.Find("Result").GetComponent<Text>().text = "CORRECT";
+                myAlpha = 1.0f;
+                resultFade = true;
                 break;
             case "Incorrect":
-                print("Incorrect");
                 Destroy(other.gameObject);
+                scoreText.text = "";
+                result = (GameObject)Instantiate(Resources.Load("Result"));
+                GameObject.Find("Result").GetComponent<Text>().text = "INCORRECT";
+                myAlpha = 1.0f;
+                resultFade = true;
                 break;
             default:
                 break;
         }      
+    }
+
+    void Update()
+    {
+        //If result is being shown, continue to fade
+        if (resultFade)
+        {
+            Text result = GameObject.Find("Result").GetComponent<Text>();
+            myAlpha = myAlpha - speedsmooth * Time.deltaTime;
+            print(myAlpha);
+            if (myAlpha > 0)
+            { 
+                result.color = new Color(1.0f, 1.0f, 1.0f, myAlpha);
+            }
+            else {
+                resultFade = false;
+            }
+        }
+        
     }
 }
