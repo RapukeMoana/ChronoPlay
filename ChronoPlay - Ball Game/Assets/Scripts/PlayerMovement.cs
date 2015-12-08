8f844c8da82479ee49ff5673073922062f4fff4d
hd910 <hd910@outlook.com>
2015-12-09 11:37:49 +1300

    Smooth out platform + code for accelerometer option
    Cant test accelerometer option

--- ChronoPlay - Ball Game/Assets/Scripts/PlayerMovement.cs
+++ ChronoPlay - Ball Game/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ using System.Text.RegularExpressions;
 public class PlayerMovement : MonoBehaviour {
     public float speed;
     public float plateDistance = 20f;
+    public bool isKeyBoard;
 
     private Rigidbody rb;
     private float speedsmooth = 0.8f;
@@ -27,14 +28,30 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
-        rb.AddForce(movement * speed);
+        //accelerometer option - but cant test 
+        if (isKeyBoard)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+            rb.AddForce(movement * speed);
+        }
+        else
+        {
+            float moveHorizontal = Input.acceleration.x;
+            float moveVertical = Input.acceleration.z;
+            print(moveVertical +" "+ moveVertical);
+            Vector3 movement = new Vector3(moveHorizontal, 0, -moveVertical);
+            if (movement.sqrMagnitude > 1)
+                movement.Normalize();
+            rb.AddForce(movement * speed);
+        }
+
 
         //x axis boundary
-        if(transform.position.x > 10.0f)
+        if (transform.position.x > 10.0f)
         {
             transform.position = new Vector3(10.0f, transform.position.y, transform.position.z);
         } else if (transform.position.x < -10.0f)
@@ -125,6 +142,9 @@ public class PlayerMovement : MonoBehaviour {
             Texture2D texture = new Texture2D(1, 1);
 
             string stageEventUri = contentItems[z].uri;
+            print(stageEventUri);
+            print(contentItems[z].mediaSource);
+            print(contentItems[z].mediaType);
 
             // Start a download of the given URL
             WWW www = new WWW(Uri.EscapeUriString(stageEventUri));
