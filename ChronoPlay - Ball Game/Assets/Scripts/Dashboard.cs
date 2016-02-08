7842f247ce5747b704f57a927bb24e7fc8ae6279
hd910 <hd910@outlook.com>
2016-02-08 23:59:56 +1300

    Link Dashboard to main game + Customisation functionality

--- ChronoPlay - Ball Game/Assets/Scripts/Dashboard.cs
+++ ChronoPlay - Ball Game/Assets/Scripts/Dashboard.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class Dashboard : MonoBehaviour {
 
@@ -12,6 +13,13 @@ public class Dashboard : MonoBehaviour {
     public GameObject loadingImage;
     public static bool menuEnabled = true;
 
+    public Slider sliderPlatform;
+    public Slider sliderHoles;
+    public Text sliderNumberOfPlatforms;
+    public Text sliderNumberOfHoles;
+
+    private SettingsConfig settings;
+
     // Use this for initialization
     void Start () {
         if(PlayerPrefs.HasKey("Last Played Date"))
@@ -21,6 +29,8 @@ public class Dashboard : MonoBehaviour {
             totalTimeText.text = PlayerPrefs.GetFloat("Total Time").ToString("n1")+"s";
             averageTimeText.text = PlayerPrefs.GetFloat("Average Time").ToString("n1")+"s";
         }
+
+        settings = new SettingsConfig();
     }
 
     // Update is called once per frame
@@ -28,11 +38,40 @@ public class Dashboard : MonoBehaviour {
 	
 	}
 
+    void OnEnable()
+    {
+        sliderPlatform.onValueChanged.AddListener(ChangeValuePlatform);
+        sliderHoles.onValueChanged.AddListener(ChangeValueHoles);
+        ChangeValuePlatform(sliderPlatform.value);
+        ChangeValueHoles(sliderPlatform.value);
+    }
+
+    void OnDisable()
+    {
+        sliderPlatform.onValueChanged.RemoveAllListeners();
+    }
+
+    void ChangeValuePlatform(float platform)
+    {
+        sliderNumberOfPlatforms.text = platform + "";
+        settings.numPlatforms = Convert.ToInt32(platform);
+    }
+
+    void ChangeValueHoles(float holes)
+    {
+        sliderNumberOfHoles.text = holes + "";
+        settings.numHoles = Convert.ToInt32(holes);
+    }
+
+
     public void StartCosmos()
     {
-        //loadingImage.SetActive(true);
-        Debug.Log("YES");
-        //SceneManager.LoadScene(1);
-        //menuEnabled = false;
+        settings.superCollection = "chronozoom";
+        settings.collection = "cosmos";
+        settings.SetPublicVariables();
+
+        loadingImage.SetActive(true);
+        SceneManager.LoadScene(1);
+        menuEnabled = false;
     }
 }
